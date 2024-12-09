b0a1eb74613e03ade80aa5b7e2d7fb6171cd7e54
EinNiemand28 <pzypzy1028@gmail.com>
2024-12-09 16:20:37 +0800

    add bootstrap

--- demo/app/models/student_event.rb
+++ demo/app/models/student_event.rb
@@ -2,7 +2,7 @@ class StudentEvent < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  enum status: { :registered => 0, :canceled => 1 }
+  enum :status, { :registered => 0, :canceled => 1 }
 
   validates :user_id, uniqueness: { scope: :event_id, message: "已报名该活动。" }
 end
